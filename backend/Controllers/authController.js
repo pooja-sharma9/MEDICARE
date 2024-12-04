75dd6420293dc3428276ad715d97db9d1ad4075d
pooja-sharma9 <poojak.sharma98038@gmail.com>
2024-12-04 09:39:33 +0530

    updated code

--- backend/Controllers/authController.js
+++ backend/Controllers/authController.js
@@ -29,9 +29,13 @@ export const register = async (req, res) => {
             return res.status(400).json({ message: 'User already exists' });
         }
 
+        // // Hash password
+        // const salt = await bcrypt.genSalt(10);
+        // const hashPassword = await bcrypt.hash(password, salt);
+
         // Hash password
-        const salt = await bcrypt.genSalt(10);
-        const hashPassword = await bcrypt.hash(password, salt);
+        const hashPassword = await bcrypt.hash(password, 10);
+
 
         if (role === 'patient') {
             user = new User({
@@ -88,7 +92,7 @@ export const login = async (req, res) => {
         console.log(bcrypt.compare); // Debugging line to check the function type
 
         // Compare the password (make sure to await the comparison)
-        const isPasswordMatch = await bcrypt.compare(password, user.password); // Ensure this line is awaited properly
+        const isPasswordMatch = bcrypt.compare(password, user.password); // Ensure this line is awaited properly
 
         if (!isPasswordMatch) {
             return res.status(400).json({ status: false, message: 'Invalid Credentials' });
